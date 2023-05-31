1f466d51efb0325e2bbcb55e1b281f114d89060d
die-goncalves <die.goncalves1990@gmail.com>
2023-05-31 10:41:22 -0300

    feat(app/fonts.ts): add font Saira_Stencil_One

--- capputeeno/src/app/fonts.ts
+++ capputeeno/src/app/fonts.ts
@@ -1,5 +1,9 @@
-import { Saira } from 'next/font/google'
+import { Saira, Saira_Stencil_One } from 'next/font/google'
 
 const saira = Saira({ subsets: ['latin'] })
+const saira_stencil_one = Saira_Stencil_One({
+  subsets: ['latin'],
+  weight: '400'
+})
 
-export { saira }
+export { saira, saira_stencil_one }
