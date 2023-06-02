d04ab3100c505451aa15353393f9a4a5ac00d86a
die-goncalves <die.goncalves1990@gmail.com>
2023-06-02 10:47:47 -0300

    feat: add ProductCard component

---
+++ capputeeno/src/components/product-card.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { formatPrice } from '@/utils/format-price'
+
+interface IProductCard {
+  id: string
+  name: string
+  price: number
+  imageURL: string
+}
+
+export function ProductCard(props: IProductCard) {
+  const priceFormatted = formatPrice(props.price)
+
+  return (
+    <Link
+      href={`/product/${props.id}`}
+      className="w-fit rounded-b rounded-t-lg overflow-hidden bg-[rgba(var(--white))]/40 border-none outline-none focus:ring-2 focus:ring-[rgb(var(--orange-low))] transition-shadow"
+    >
+      <div className="relative w-[256px] h-[300px]">
+        <Image
+          src={props.imageURL}
+          alt={props.name}
+          fill
+          className="object-cover"
+          sizes="(max-width: 768px) 70vw, (min-width: 769px) 30vw"
+        />
+      </div>
+
+      <div className="flex flex-col px-3 py-2 gap-2">
+        <span className="text-base leading-normal font-light">
+          {props.name}
+        </span>
+        <hr className="h-[1px] bg-[rgb(var(--text-secondary-rgb))]" />
+        <span className="text-sm leading-normal font-semibold text-[rgb(var(--text-tertiary-rgb))]">
+          {priceFormatted}
+        </span>
+      </div>
+    </Link>
+  )
+}
