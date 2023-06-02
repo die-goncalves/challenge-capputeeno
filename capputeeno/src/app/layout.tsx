ed8b369de1ebb81f8ea231d3c034a73ba4c2c35d
die-goncalves <die.goncalves1990@gmail.com>
2023-06-02 10:19:43 -0300

    feat(app/layout.tsx): update root layout with header

--- capputeeno/src/app/layout.tsx
+++ capputeeno/src/app/layout.tsx
@@ -1,4 +1,9 @@
 import './globals.css'
+
+import { Suspense } from 'react'
+import { Logo } from '@/components/logo'
+import { SearchBar } from '@/components/search-bar'
+import { BagIcon } from '@/components/icons/bag-icon'
 import { saira } from './fonts'
 
 export const metadata = {
@@ -13,7 +18,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt" className={saira.className}>
-      <body>{children}</body>
+      <body>
+        <header className="w-full h-[80px] px-40 bg-[rgba(var(--white))]/95 sticky top-0 z-10 backdrop-blur shadow-[0px_4px_12px_0px_rgba(0,_0,_0,_0.1)]">
+          <nav className="w-full h-full flex justify-between items-center">
+            <Logo />
+            <div className="flex gap-4 items-center">
+              <Suspense>
+                <SearchBar />
+              </Suspense>
+              <BagIcon className="stroke-[rgb(var(--text-secondary-rgb))] w-6 h-6" />
+            </div>
+          </nav>
+        </header>
+
+        {children}
+      </body>
     </html>
   )
 }
