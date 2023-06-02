ca38acb081bd7a3f05aaf2002669e46f278ce414
die-goncalves <die.goncalves1990@gmail.com>
2023-06-02 12:06:14 -0300

    feat(app/layout.tsx): add CartContextProvider

--- capputeeno/src/app/layout.tsx
+++ capputeeno/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Suspense } from 'react'
 import { Logo } from '@/components/logo'
 import { SearchBar } from '@/components/search-bar'
 import { BagIcon } from '@/components/icons/bag-icon'
+import { CartContextProvider } from '@/contexts/cart-context'
 import { saira } from './fonts'
 
 export const metadata = {
@@ -19,19 +20,21 @@ export default function RootLayout({
   return (
     <html lang="pt" className={saira.className}>
       <body>
-        <header className="w-full h-[80px] px-40 bg-[rgba(var(--white))]/95 sticky top-0 z-10 backdrop-blur shadow-[0px_4px_12px_0px_rgba(0,_0,_0,_0.1)]">
-          <nav className="w-full h-full flex justify-between items-center">
-            <Logo />
-            <div className="flex gap-4 items-center">
-              <Suspense>
-                <SearchBar />
-              </Suspense>
-              <BagIcon className="stroke-[rgb(var(--text-secondary-rgb))] w-6 h-6" />
-            </div>
-          </nav>
-        </header>
+        <CartContextProvider>
+          <header className="w-full h-[80px] px-40 bg-[rgba(var(--white))]/95 sticky top-0 z-10 backdrop-blur shadow-[0px_4px_12px_0px_rgba(0,_0,_0,_0.1)]">
+            <nav className="w-full h-full flex justify-between items-center">
+              <Logo />
+              <div className="flex gap-4 items-center">
+                <Suspense>
+                  <SearchBar />
+                </Suspense>
+                <BagIcon className="stroke-[rgb(var(--text-secondary-rgb))] w-6 h-6" />
+              </div>
+            </nav>
+          </header>
 
-        {children}
+          {children}
+        </CartContextProvider>
       </body>
     </html>
   )
