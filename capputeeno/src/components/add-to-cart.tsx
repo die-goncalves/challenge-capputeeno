067bfc36ae4fe53042398e46c35d2795372db9cf
die-goncalves <die.goncalves1990@gmail.com>
2023-06-02 12:12:50 -0300

    feat(components/add-to-cart.tsx): add logic to add product to cart

--- capputeeno/src/components/add-to-cart.tsx
+++ capputeeno/src/components/add-to-cart.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useCartDispatch } from '@/contexts/cart-context'
 import { BagIcon } from './icons/bag-icon'
 
 interface IAddToCart {
@@ -8,8 +11,29 @@ interface IAddToCart {
   price_in_cents: number
 }
 export function AddToCart(props: IAddToCart) {
+  const { id, name, description, image_url, price_in_cents } = props
+  const { dispatch } = useCartDispatch()
+
+  function handleAddProduct() {
+    dispatch({
+      type: 'add',
+      payload: {
+        product: {
+          id,
+          name,
+          description,
+          image_url,
+          price_in_cents
+        }
+      }
+    })
+  }
+
   return (
-    <button className="flex mt-auto items-center justify-center gap-3 w-full h-11 bg-[rgb(var(--brand-blue-rgb))] rounded outline-none focus:ring-2 focus:ring-[rgb(var(--orange-low))] transition-shadow">
+    <button
+      onClick={handleAddProduct}
+      className="flex mt-auto items-center justify-center gap-3 w-full h-11 bg-[rgb(var(--brand-blue-rgb))] rounded outline-none focus:ring-2 focus:ring-[rgb(var(--orange-low))] transition-shadow"
+    >
       <BagIcon className="stroke-[rgb(var(--shape-light-rgb))] w-6 h-6" />
       <span className="uppercase font-medium text-[rgb(var(--shape-light-rgb))]">
         Adicionar ao carrinho
