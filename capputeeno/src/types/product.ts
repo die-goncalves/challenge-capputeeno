9c24500032e0a4983c274e871569759ae1fcfd37
die-goncalves <die.goncalves1990@gmail.com>
2023-06-02 10:07:50 -0300

    feat: add Product type

---
+++ capputeeno/src/types/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  category: string
+  created_at: string
+  description: string
+  id: string
+  image_url: string
+  name: string
+  price_in_cents: number
+  sales: number
+}
